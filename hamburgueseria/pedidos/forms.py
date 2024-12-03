ae590ab26a4b132718da8df4ea3e4ffde24995e8
Joaquin Benitez <joaquincdelu@gmail.com>
2024-12-03 20:50:19 -0300

    correciones en pedidos

--- hamburgueseria/pedidos/forms.py
+++ hamburgueseria/pedidos/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Pedido
+from menu_app.models import Hamburguesa
 
 class PedidoForm(forms.ModelForm):
     class Meta:
         model = Pedido
         fields = ['nombre_usuario', 'email', 'direccion', 'hamburguesa', 'cantidad']
 
+    hamburguesa = forms.ModelChoiceField(queryset=Hamburguesa.objects.all(), empty_label="Selecciona una hamburguesa")
